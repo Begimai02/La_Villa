ca082bf35016b5d33f5e9feec2826705d8683238
unknown <anarbekovaz@gmail.com>
2021-02-22 13:27:39 +0600

    new changes from Jibek

---
+++ src/Routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header/Header';
+
+ const Routes = () => {
+     return (
+         <BrowserRouter>
+            <Header>
+                
+            </Header>
+         </BrowserRouter>
+     );
+ };
+ 
+ export default Routes;
