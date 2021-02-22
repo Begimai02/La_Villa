027d871a29e888a624d7b890d8a18dfa3e758d45
unknown <tilek1199@gmail.com>
2021-02-22 20:35:41 +0600

    created files, add, read

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import VillaList from './components/Body/VillaList';
 import Header from './components/Header/Header';
 import VillasContextProvider from './contexts/VillaContext';
+import AddVilla from './Pages/Admin/AddVilla/AddVilla'
 
- const Routes = () => {
-     return (
-         <VillasContextProvider>
+const Routes = () => {
+    return (
+        <VillasContextProvider>
             <BrowserRouter>
-                <Header>
-                    <Switch>
-                        <Route>
-
-                        </Route>
-                    </Switch>
-                </Header>
+                <Switch>
+                    <Route exact path="/list" component={VillaList} />
+                    <Route exact path="/add" component={AddVilla} />
+                </Switch>
             </BrowserRouter>
-         </VillasContextProvider>
-     );
- };
- 
- export default Routes;
+        </VillasContextProvider>
+    );
+};
+
+export default Routes;
