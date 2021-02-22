8901968e5c9b93a84a6aa929a35617cd97240747
Taalai <taalai09@mail.ru>
2021-02-22 15:52:10 +0600

    credit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
-import './App.css';
-import CreditCard from './components/Payment/CreditCard';
-=======
 import Routes from "./Routes";
+import './App.css'
 
->>>>>>> e052e24d039e1f620599e54cebe110834d61bb7e
 
 function App() {
   return (
     <div className="App">
-     <CreditCard/>
+     <Routes/>
 
     </div>
   )
