062d932f37ac1b7fabf27d79b21b50a52cfe3a20
Taalai <taalai09@mail.ru>
2021-02-22 15:43:40 +0600

    CreditCard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import CreditCard from './components/Payment/CreditCard';
 
 function App() {
   return (
     <div className="App">
-     
+     <CreditCard/>
+
     </div>
   );
 }
