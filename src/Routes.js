39912e70ced9027bdb3060a53776f299f24f54fb
unknown <anarbekovaz@gmail.com>
2021-02-22 14:51:56 +0600

    ndf

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
+import VillasContextProvider from './contexts/VillaContext';
 
  const Routes = () => {
      return (
-         <BrowserRouter>
-            <Header>
-                <Switch>
-                    <Route>
-                        
-                    </Route>
-                </Switch>
-            </Header>
-         </BrowserRouter>
+         <VillasContextProvider>
+            <BrowserRouter>
+                <Header>
+                    <Switch>
+                        <Route>
+
+                        </Route>
+                    </Switch>
+                </Header>
+            </BrowserRouter>
+         </VillasContextProvider>
      );
  };
  
